d26d19c5ecfd0e94206f9353f36ab110ce7332ef
Aditya21074 <adityacse21074@gmail.com>
2024-09-26 22:58:28 +0530

    Created signup and login pages

---
+++ src/main/java/com/learnpath/LearnPath/LearnPathApplication.java
@@ -0,0 +1,13 @@
+package com.learnpath.LearnPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnpath/LearnPath/LearnPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnpath.LearnPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
